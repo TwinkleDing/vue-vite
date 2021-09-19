2ba391d5d53389ea7321e1b029e194929914f411
twinkleding <32945061+TwinkleDing@users.noreply.github.com>
2021-09-19 18:19:19 +0800

    增加node服务器

--- node/app.js
+++ node/app.js
@@ -1,29 +1,22 @@
-const Koa = require( "koa" );
+const Koa = require( 'koa' );
+
+const bodyParser = require( 'koa-bodyparser' );
+
+const controller = require( './controller' );
 
 const app = new Koa();
-app.use( async ( ctx, next ) => {
-    console.log( `${ctx.request.method} ${ctx.request.url}` ); // 打印URL
-    await next(); // 调用下一个middleware
-} );
 
+// log request URL:
 app.use( async ( ctx, next ) => {
-    const start = new Date().getTime(); // 当前时间
-    await next(); // 调用下一个middleware
-    const ms = new Date().getTime() - start; // 耗费时间
-    console.log( `Time: ${ms}ms` ); // 打印耗费时间
-} );
-
-app.use( async ( ctx, next ) => {
+    console.log( `Process ${ctx.request.method} ${ctx.request.url}...` );
     await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
 } );
 
-// app.use( async ( ctx, next ) => {
-//     if ( await checkUserPermission( ctx ) ) {
-//         await next();
-//     } else {
-//         ctx.response.status = 403;
-//     }
-// } );
+// parse request body:
+app.use( bodyParser() );
+
+// add controllers:
+app.use( controller() );
+
 app.listen( 3333 );
+console.log( 'app started at port 3333...' );
