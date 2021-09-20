afbf040c8419195a4390964e18198c2881c0f178
twinkleding <32945061+TwinkleDing@users.noreply.github.com>
2021-09-20 14:21:38 +0800

    vite

--- draw-guess/src/App.vue
+++ draw-guess/src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <Game msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import Game from "./view/game.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
     name: "App",
     components: {
-        Game
-    }
+        HelloWorld,
+    },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
