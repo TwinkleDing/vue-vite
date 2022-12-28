de3b0b8ce60c922427ce10c8e76084d286818db5
dingyuliang <dingyuliang>
2022-12-28 18:19:27 +0800

    绘画修改

--- draw-guess/src/view/game.vue
+++ draw-guess/src/view/game.vue
@@ -2,7 +2,7 @@
   <div id="game" @mouseup="drawEnd">
     <div>
       <canvas
-        v-show="drawer"
+        v-show="isDrawer"
         id="canvas"
         class="canvas"
         height="800"
@@ -11,13 +11,13 @@
         @mousemove="drawing"
       />
       <canvas
-        v-show="!drawer"
+        v-show="!isDrawer"
         id="canvasAnswer"
         class="canvas"
         height="800"
         width="1000"
       />
-      <div v-if="drawer">
+      <div v-if="isDrawer">
         <div v-if="status === 'Started'" class="flex">
           <color-list @set-line-color="setLineColor" />
           <width-list @set-line-width="setLineWidth" />
@@ -28,6 +28,7 @@
           <el-button @click="nextUser">更换玩家</el-button>
         </div>
       </div>
+      <div v-if="status === 'Started' && !isDrawer">当前玩家：{{ drawer }}</div>
     </div>
     <div>
       <div class="answer-list">
@@ -37,9 +38,15 @@
           <span>{{ item.answer }}</span>
         </div>
       </div>
-      <div>
-        <div>提示：<el-tag>2个字</el-tag>；<el-tag>动物</el-tag></div>
-        <div v-if="!drawer" class="flex">
+      <div v-if="status === 'Started'">
+        <div>
+          <div v-if="isDrawer">题目：{{ question }}</div>
+          <div>
+            提示：
+            <el-tag v-for="(item, index) in prompt" :key="index">{{ item }}</el-tag>
+          </div>
+        </div>
+        <div v-if="!isDrawer" class="flex">
           <el-input
             @keyup.enter="submit"
             v-model="answerInput"
@@ -78,11 +85,14 @@ export default {
       answerInput: "",
       answerList: [],
       user: "",
-      drawer: localStorage.getItem("user") === "twinkleding",
+      drawer: "",
+      isDrawer: localStorage.getItem("user") === "twinkleding",
       question: "",
       lineColor: "#000",
       lineWidth: "1",
       status: StatusList[0],
+      prompt: [],
+      questionTimer: null,
     };
   },
   mounted() {
@@ -92,19 +102,24 @@ export default {
       const ctx = canvasAnswer.getContext("2d");
       const client = JSON.parse(msg.data);
       // 玩家加入游戏
-      if (client.status === "user") {
+      if (client.status === "User") {
         this.answerList.push({
           user: "系统",
           answer: `欢迎${client.user}加入游戏`,
         });
         return;
       }
-
-      if (client.status === "qusetionUser") {
-        this.drawer = client.user === this.user;
+      if (client.status === "NextUser") {
+        if (client.user === this.user) {
+          this.nextUser();
+          return;
+        }
+        this.startGame();
+        this.drawer = client.user;
+        this.isDrawer = client.user === this.user;
       }
       // 提交答案
-      if (client.status === "answer") {
+      if (client.status === "Answer") {
         this.answerList.push({
           user: client.user,
           answer: client.answer,
@@ -112,8 +127,20 @@ export default {
       }
       // 开始游戏，获取问题
       if (client.status === "StartGame") {
+        this.drawer = client.user;
         this.status = StatusList[1];
         this.question = client.question.split("，")[0];
+        // 清空之前遗留的
+        clearTimeout(this.questionTimer);
+        this.prompt = [];
+        // 开始计时给提示
+        this.questionTimer = setTimeout(() => {
+          this.prompt.push(client.question.split("，")[1]);
+          clearTimeout(this.questionTimer);
+          this.questionTimer = setTimeout(() => {
+            this.prompt.push(client.question.split("，")[2]);
+          }, 20000);
+        }, 20000);
       }
       // 清空画板
       if (client.status === "Empty") {
@@ -122,14 +149,21 @@ export default {
         return;
       }
       // 绘制途中
-      if (["start", "drawing"].includes(client.status)) {
-        if (client.status === "drawing") {
+      if (["Start", "Drawing"].includes(client.status)) {
+        if (client.status === "Drawing") {
           ctx.moveTo(this.mouseLastX, this.mouseLastY);
-        } else if (client.status === "start") {
+        } else if (client.status === "Start") {
           ctx.moveTo(client.x, client.y);
         }
-        drawAnswerCanvas(ctx, client);
+        this.drawAnswerCanvas(ctx, client);
       }
+    };
+    window.onbeforeunload = () => {
+      const client = {
+        status: "Onunload",
+        user: this.user,
+      };
+      this.sendClient(client);
     };
   },
   methods: {
@@ -147,7 +181,7 @@ export default {
           }).then(({ value }) => {
             localStorage.setItem("user", value);
             const client = {
-              status: "user",
+              status: "User",
               user: value,
             };
             this.user = value;
@@ -155,7 +189,7 @@ export default {
           });
         } else {
           const client = {
-            status: "user",
+            status: "User",
             user: this.user,
           };
           this.sendClient(client);
@@ -174,18 +208,25 @@ export default {
     startGame() {
       const client = {
         status: "StartGame",
+        user: this.user,
       };
       this.sendClient(client);
     },
     //下一个玩家进行游戏
-    nextUser() {},
+    nextUser() {
+      clearTimeout(this.questionTimer);
+      const client = {
+        status: "NextUser",
+      };
+      this.sendClient(client);
+    },
     // 提交答案
     submit() {
       if (this.answerInput === "") {
         return;
       }
       const client = {
-        status: "answer",
+        status: "Answer",
         user: this.user,
         answer: this.answerInput,
       };
@@ -210,13 +251,13 @@ export default {
       this.mouseBeginX = e.clientX - this.canvas.offsetLeft;
       this.mouseBeginY = e.clientY - this.canvas.offsetTop;
       this.context.moveTo(this.mouseBeginX, this.mouseBeginY);
-      this.openDraw(e, "start");
+      this.openDraw(e, "Start");
       this.start = true;
     },
     // 绘制中
     drawing(e) {
       if (this.start) {
-        this.openDraw(e, "drawing");
+        this.openDraw(e, "Drawing");
       }
     },
     // 绘制结束
--- node/app.js
+++ node/app.js
@@ -30,17 +30,36 @@ const WebSocketServer = WebSocket.Server;
 const wss = new WebSocketServer({
 	port: 3001,
 });
+// 参加游戏玩家列表
+let userList = [];
 wss.on("connection", function (ws) {
 	ws.on("message", function (message) {
 		const msg = JSON.parse(message.toString());
 		console.log(msg.status);
 		if (msg.status === "StartGame") {
-            let arr = fs.readFileSync("question.txt").toString().split("\n");
-            let question = arr[Math.floor(Math.random() * arr.length)]
-            console.log(question)
-            msg.question = question;
-        }
-        message = JSON.stringify(msg);
+			let arr = fs.readFileSync("question.txt").toString().split("\n");
+			let question = arr[Math.floor(Math.random() * arr.length)];
+			msg.question = question;
+		}
+		if (msg.status === "User") {
+			if (!userList.includes(msg.user)) {
+				userList.push(msg.user);
+			}
+		}
+		if (msg.status === "Onunload") {
+			let index = "";
+			userList.forEach((item, ind) => {
+				if (item.user === msg.user) {
+					index = ind;
+				}
+			});
+			userList.splice(index, 1);
+		}
+		if (msg.status === "NextUser") {
+			let index = Math.floor(Math.random() * userList.length);
+			msg.user = userList[index];
+		}
+		message = JSON.stringify(msg);
 		wss.clients.forEach(function each(client) {
 			client.send(`${message}`);
 		});
