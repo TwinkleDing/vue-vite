2ba617cdbfe96024fd4eba6fc5f626069dcabf6a
twinkleding <twinkle415@163.com>
2023-01-03 22:35:19 +0800

    App.vue的element-plus文件引入报错修改

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,6 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'element-plus/dist/locale/zh-cn.mjs';
+declare module 'element-plus/dist/locale/en.mjs';
