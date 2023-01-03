8351ae86a35e66c179eba09f4620e4cdd1c529e9
dingyuliang <dingyuliang>
2023-01-03 18:23:04 +0800

    代码整理

--- node/app.js
+++ node/app.js
@@ -2,7 +2,6 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const controller = require("./controller");
 const cors = require("koa2-cors");
-const fs = require("fs");
 const app = new Koa();
 app.use(cors());
 
@@ -18,62 +17,5 @@ app.use(bodyParser());
 // add controllers:
 app.use(controller());
 
-app.listen(3333);
-console.log("app started at port 3333...");
-
-const WebSocket = require("ws");
-
-// 引用Server类:
-const WebSocketServer = WebSocket.Server;
-
-// 实例化:
-const wss = new WebSocketServer({
-	port: 3001,
-});
-// 参加游戏玩家列表
-let userList = [];
-wss.on("connection", function (ws) {
-	ws.on("message", function (message) {
-		const msg = JSON.parse(message.toString());
-		if (msg.status === "User") {
-			console.log(msg.user);
-		}
-		if (msg.status === "StartGame") {
-			let arr = fs.readFileSync("question.txt").toString().split("\n");
-			let question = arr[Math.floor(Math.random() * arr.length)];
-			msg.question = question;
-		}
-		if (msg.status === "User") {
-			if (!userList.includes(msg.user)) {
-				userList.push(msg.user);
-			}
-		}
-		if (msg.status === "Onunload") {
-			let index = "";
-			userList.forEach((item, ind) => {
-				if (item.user === msg.user) {
-					index = ind;
-				}
-			});
-			userList.splice(index, 1);
-		}
-		if (msg.status === "NextUser") {
-			let index = Math.floor(Math.random() * userList.length);
-			msg.user = userList[index];
-		}
-		wss.clients.forEach(function each(client) {
-			if (msg.status === "User") {
-				let userMsg = JSON.parse(JSON.stringify(msg));
-				userList.forEach((item) => {
-					userMsg.user = item;
-					client.send(`${JSON.stringify(userMsg)}`);
-				});
-			} else {
-				if (msg.status === "NextUser") {
-					console.log(123123);
-				}
-				client.send(`${JSON.stringify(msg)}`);
-			}
-		});
-	});
-});
+app.listen(668);
+console.log("app started at port 668...");
---
+++ node/draw.js
@@ -0,0 +1,54 @@
+const WebSocket = require("ws");
+const fs = require("fs");
+
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
+
+// 实例化:
+const wss = new WebSocketServer({
+	port: 678,
+});
+console.log("app started at port 678...");
+
+// 参加游戏玩家列表
+let userList = [];
+wss.on("connection", function (ws) {
+	ws.on("message", function (message) {
+		const msg = JSON.parse(message.toString());
+		if (msg.status === "StartGame") {
+			let arr = fs.readFileSync("question.txt").toString().split("\n");
+			let question = arr[Math.floor(Math.random() * arr.length)];
+			msg.question = question;
+		}
+		if (msg.status === "User") {
+			if (!userList.includes(msg.user)) {
+				userList.push(msg.user);
+			}
+		}
+		if (msg.status === "Onunload") {
+			let index = "";
+			userList.forEach((item, ind) => {
+				if (item.user === msg.user) {
+					index = ind;
+				}
+			});
+			userList.splice(index, 1);
+		}
+		if (msg.status === "NextUser") {
+			let index = Math.floor(Math.random() * userList.length);
+			msg.user = userList[index];
+		}
+		wss.clients.forEach(function each(client) {
+			if (msg.status === "User") {
+				let userMsg = JSON.parse(JSON.stringify(msg));
+				userList.forEach((item) => {
+					userMsg.user = item;
+					client.send(`${JSON.stringify(userMsg)}`);
+				});
+			} else {
+				client.send(`${JSON.stringify(msg)}`);
+			}
+		});
+	});
+});
+
