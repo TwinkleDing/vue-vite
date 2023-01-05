4f6d62bcbda464e383bb0c532e0c90b5d9b58655
dingyuliang <dingyuliang>
2023-01-05 11:46:58 +0800

    修改构建脚本

--- pack-build.sh
+++ pack-build.sh
@@ -1,36 +1,12 @@
 echo '---------- 构建主应用 ----------'
-cd main-application
+cd main-vue-application
 yarn
 yarn build
-
-# echo '---------- 打包主应用 ----------'
-# cd ../
-# cd dist
-
-# echo '---------- 清除上次的包 ----------'
-# rm ./MainApp.tar.gz
-
-# echo '---------- 开始打包 ----------'
-# tar -zcvf ./MainApp.tar.gz ./MainApp
-
-# echo '---------- 打包结束 ----------'
 
 echo '---------- 主应用打包完成，回到根目录 ----------'
 cd ../
 
 echo '---------- 构建微应用 ----------'
-cd micro-application
+cd micro-vue-application
 yarn
 yarn build
-
-# echo '---------- 打包微应用 ----------'
-# cd ../
-# cd dist
-
-# echo '---------- 清除上次的包 ----------'
-# rm ./MicroApp.tar.gz
-
-# echo '---------- 开始打包 ----------'
-# tar -zcvf ./MicroApp.tar.gz ./MicroApp
-
-# echo '---------- 打包结束 ----------'
