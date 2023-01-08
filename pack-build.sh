041ff85042c795a5bb81d9fd52745880807f2cc0
twinkleding <twinkle415@163.com>
2023-01-08 14:26:18 +0800

    新增react子应用一键打包并且修改打包为自动读取文件夹内容

--- pack-build.sh
+++ pack-build.sh
@@ -10,3 +10,12 @@ echo '---------- 构建vue微应用 ----------'
 cd micro-vue-application
 yarn
 yarn build
+
+
+echo '---------- vue微应用打包完成，回到根目录 ----------'
+cd ../
+
+echo '---------- 构建react微应用 ----------'
+cd micro-react-application
+yarn
+yarn build
--- zip.js
+++ zip.js
@@ -1,60 +1,73 @@
 const fs = require("fs");
+const path = require("path");
 const archiver = require("archiver");
 const chalk = require("chalk");
+const distPath = "./dist";
 
 // 开始进入打包
 console.log(chalk.blue.bgGreen("---------- Compress Start ----------"));
 
-// 打包文件
-zipApp("Main-Vue-App", "MainVueApp"); // 打包vue主应用
-zipApp("Micro-Vue-App", "MicroVueApp"); // 打包vue微应用
+const hasDir = fs.existsSync(distPath);
+hasDir &&
+  fs.readdirSync(distPath).map((fileName) => {
+    const isMicroApp = isDir(fileName);
+    if (isMicroApp) {
+      zipApp(fileName); // 打包应用
+    }
+  });
 
-function zipApp(inName, outName) {
-	const archive = archiver("zip", {
-		zlib: { level: 9 }, // 设置压缩级别
-	});
+function isDir(fileName) {
+  return !fs.lstatSync(fileName).isFile();
+}
 
-	// 创建文件输出流
-	const output = fs.createWriteStream(__dirname + `/dist/${outName}.zip`);
+function zipApp(fileName) {
+  const archive = archiver("zip", {
+    zlib: { level: 9 }, // 设置压缩级别
+  });
 
-	// 文件输出流结束
-	output.on("close", function () {
-		console.log(
-			chalk.yellow(
-				`${inName} total szie ${archive
-					.pointer()
-					.toString()
-					.replace(/\B(?=(\d{3})+\b)/g, ",")} bytes`
-			)
-		);
-		console.log(chalk.blue.bgGreen(`Archiver compressed ${inName} End`));
-	});
+  // 创建文件输出流
+  const output = fs.createWriteStream(__dirname + `/dist/${fileName}.zip`);
 
-	// 数据源是否耗尽
-	output.on("end", function () {
-		console.log(chalk.red("Data source to exhaust"));
-	});
+  // 文件输出流结束
+  output.on("close", function () {
+    console.log(
+      chalk.yellow(
+        `${fileName} total szie ${archive
+          .pointer()
+          .toString()
+          .replace(/\B(?=(\d{3})+\b)/g, ",")} bytes`
+      )
+    );
+    console.log(chalk.blue.bgGreen(`Archiver compressed ${fileName} End`));
+  });
 
-	// 通过管道方法将输出流存档到文件
-	archive.pipe(output);
+  // 数据源是否耗尽
+  output.on("end", function () {
+    console.log(chalk.red("Data source to exhaust"));
+  });
 
-	// 打包dist里面的所有文件和目录
-	archive.directory(`dist/${inName}`, false);
+  // 通过管道方法将输出流存档到文件
+  archive.pipe(output);
 
-	// 打包警告
-	archive.on("warning", function (err) {
-		if (err.code === "ENOENT") {
-			console.log(chalk.bgYellow("Stat failures and other non-blocking errors"));
-		} else {
-			throw err;
-		}
-	});
+  // 打包dist里面的所有文件和目录
+  archive.directory(`${distPath}/${fileName}`, false);
 
-	// 打包错误
-	archive.on("error", function (err) {
-		throw err;
-	});
+  // 打包警告
+  archive.on("warning", function (err) {
+    if (err.code === "ENOENT") {
+      console.log(
+        chalk.bgYellow("Stat failures and other non-blocking errors")
+      );
+    } else {
+      throw err;
+    }
+  });
 
-	// 完成归档
-	archive.finalize();
+  // 打包错误
+  archive.on("error", function (err) {
+    throw err;
+  });
+
+  // 完成归档
+  archive.finalize();
 }
