ab7924be5c150201970184340d6c9cecb3881854
twinkleding <twinkle415@163.com>
2023-02-17 15:42:41 +0800

    添加bpmn工具

--- src/views/media/index.vue
+++ src/views/media/index.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>1</div>
+  <div class="media">
+    <div id="container" ref="container"></div>
+  </div>
 </template>
-<script lang="ts"></script>
+<script setup lang="ts">
+import { markRaw, onMounted, ref } from "vue";
+import BpmnJS from "bpmn-js";
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import BpmnModeler from "bpmn-js/lib/Modeler";
+import { bmpmStr } from "./bpmnStr.js";
 
-<style lang="scss" scoped></style>
+let bpmnModeler: any = null;
+const init: void = async () => {
+  bpmnModeler = new BpmnModeler({ container: "#container" });
+
+  try {
+    const result = await bpmnModeler.importXML(bmpmStr);
+    const { warnings } = result;
+    console.log(warnings);
+  } catch (err) {
+    console.log(err.message, err.warnings);
+  }
+};
+
+onMounted(() => {
+  init();
+});
+</script>
+
+<style lang="scss" scoped>
+.media {
+  height: 100%;
+  width: 100%;
+  #container {
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
